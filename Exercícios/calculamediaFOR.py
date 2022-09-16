a7e133316b348ba16779a11d564e74000985f85e
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-16 19:01:28 -0300

    Delete calculamediaFOR.py

--- Exercícios/calculamediaFOR.py
+++
@@ -1,8 +0,0 @@
-# Calcula media 30 alunos
-for cont in range(1,31):
-    print('\n***Aluno {} ***' .format(cont))
-    n1 = float(input("Informe a primeira nota: "))
-    n2 = float(input("Informe a segunda nota: "))
-    media = (n1 + n2) / 2
-    print("Media das notas {} e {} = {}" .format(n1,n2,media))
-    
