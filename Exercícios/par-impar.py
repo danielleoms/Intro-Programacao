c34b2450ff318b9ee7a92464cf9040ed33184c15
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-16 18:56:28 -0300

    Delete par-impar.py

--- Exercícios/par-impar.py
+++
@@ -1,8 +0,0 @@
-#Descobrir qu o número é ímpar ou par
-num= int(input('Informe um número inteiro'))
-resto= num % 2
-if resto ==0:
-    print('O seu número é par')
-else:
-    print('O seu número é ímpar')
-
