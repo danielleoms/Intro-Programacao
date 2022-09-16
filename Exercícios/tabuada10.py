ffbac49c3b20287d1d8e28d62f791614a441fbad
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-16 19:07:05 -0300

    Delete tabuada10.py

--- Exercícios/tabuada10.py
+++
@@ -1,6 +0,0 @@
-#Faça um programa que imprima a tabuada de 10
-# utilizando estrutura de repetição.
-
-for cont in range (0,11):
-    resultado = 10 * cont
-    print('10 * {} é igual a {}' .format(cont,resultado))
