cb3725d0f4aa6a23ad7cc46fc0f909cf67d70c5b
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-23 18:55:09 -0300

    Quadrado da diferença de dois números

---
+++ Exercícios/Lista de exercícios 2/exercicio1.py
@@ -0,0 +1,7 @@
+#Faça um programa para ler dois inteiros e imprimir o resultado do quadrado da
+# diferença do primeiro valor pelo segundo.
+
+num1 = int(input('Primeiro número:'))
+num2 = int(input('Segundo número:'))
+cal = (num1 - num2)**2
+print('O resultado do quadrado da diferença de {} e {} é {}'.format(num1,num2,cal))
