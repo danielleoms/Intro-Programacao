58cca0bf6590d76657e028b5af92823329630099
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-24 11:25:07 -0300

    Delete Exercícios directory

--- Exercícios/Aula 5/10inteiros.py
+++
@@ -1,8 +0,0 @@
-#Escrever um programa que lê 10 valores inteiros para a variável “a”,
-# #um de cada vez, e conta quantos destes valores são negativos,
-# exibindo esta informação na tela.
-
-for cont in range (0,11):
-    print('\n A tabuada de {}' .format(cont))
-    for x in range (0,11):
-        print('{} * {} = {}' .format(cont,x,cont*x))
--- Exercícios/Lista de exercícios 1/exercicio1.py
+++
@@ -1,9 +0,0 @@
-# Elabore um programa em Python que leia a medida de um raio de um círculo e efetue o cálculo da sua área,
-# exibindo o resultado ao final. (dica: use math.pi )
-#A área de um círculo é pi vezes o raio elevado ao quadrado (A = π r²).
-
-import math
-r = float(input('Informe o raio do círculo:'))
-área = math.pi * (r ** 2)
-
-print('A aréa é de {}' .format(área))
