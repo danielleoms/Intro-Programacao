dbce33fb9aba17a119abea6550dcf5a36fec514b
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-24 11:23:15 -0300

    Analisando triângulos

--- Lista de exercícios 1/exercicio6.py
+++ Lista de exercícios 1/exercicio6.py
@@ -1,7 +1,6 @@
 # Elaborar um programa que solicita a entrada de 3 valores (a, b, c) e verifica se esses valores podem formar ou não um triângulo.
 # Você deve considerar que os valores lidos são inteiros e positivos.
-# Caso os valores formem um triângulo, exiba essa informação e o valor do perímetro deste triângulo.
-# Se não formarem triângulo, apenas exiba uma mensagem com essa informação.
+# Caso os valores formem um triângulo, exiba essa informação e o valor do perímetro deste triângulo. Se não formarem triângulo, apenas exiba uma mensagem com essa informação.
 # (Obs.: Para formar um triângulo, cada suposto lado deve ser menor do que a soma dos outros dois lados.)
 
 a = int(input('Informe o valor de a:'))
