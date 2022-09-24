a427b0c8605f9e06baf99ed17dfa57ad3953df80
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-24 11:15:48 -0300

    Conversor de moeda: dólar X real

--- Exercícios separados/EstruturaSequencialMoeda.py
+++ Exercícios separados/EstruturaSequencialMoeda.py
@@ -1,6 +1,5 @@
 #Faça um programa em Python que leia um valor em dólar e efetue a apresentação do valor convertido em real
-#O programa deve solicitar ao usuário o valor da cotação do dólar
-#e a quantidade de dólares disponível, para que seja apresentado o valor em moeda brasileira.
+#O programa deve solicitar ao usuário o valor da cotação do dólar e a quantidade de dólares disponível, para que seja apresentado o valor em moeda brasileira.
 
 cotdol= float(input('Valor da cotação do dólar hoje em relação ao real:'))
 dolar = float(input('Quantos dólares ($) você tem na carteira?'))
