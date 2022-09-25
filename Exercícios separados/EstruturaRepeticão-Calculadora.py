4c44e9f02a8327dc9f3f4b9948e8d5e2c80bb1f8
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-24 21:58:52 -0300

    Create  EstruturaRepeticão-Calculadora.py

---
+++ Exercícios separados/EstruturaRepeticão-Calculadora.py
@@ -0,0 +1,40 @@
+
+#Escreva o menu de opções abaixo. Leia a opção do usuário,
+# leia os numeros e execute a operação escolhida. Escreva uma mensagem de erro se a opção for inválida.
+
+
+print('-=-' * 10 )
+print('        CALCULADORA')
+print('-=-' * 10 )
+
+num1 = int(input('Primeiro valor:'))
+num2 = int(input('Segundo valor:'))
+
+opcao = 0
+while opcao != 5:
+    print('-=-' * 10)
+    print('''Escolha a opção desejada:
+    [ 1 ] Soma de 2 números inteiros
+    [ 2 ] Diferença entre 2 números 
+    [ 3 ] Produto entre 2 números
+    [ 4 ] Divisão entre 2 números (o denominador não poderá ser zero)
+    [ 5 ] Sair ''')
+    opcao = int(input('Sua opção:'))
+    if opcao == 1:
+        print('{} + {} = {}'.format(num1, num2, num1 + num2))
+    elif opcao == 2:
+        if num1 > num2:
+            print(f'{num1} - {num2} = {num1 - num2}')
+        if num1 < num2:
+            print(f'{num2} - {num1} = {num2 - num1}')
+    elif opcao == 3:
+        print('{} X {} ='.format(num1, num2, num1 * num2))
+    elif opcao == 4:
+        if num1 == 0:
+            print(f'{num1} ÷ {num2} = {num1 / num2}')
+        if num2 == 0:
+            print(f'{num2} ÷ {num1} = {num2 / num1}')
+    elif opcao == 5:
+        print('Fim do progama! Volte sempre!')
+    else:
+        print('Por favor, escolha uma opção válida.')
