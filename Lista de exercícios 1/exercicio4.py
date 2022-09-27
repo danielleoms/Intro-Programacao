dc718da6a3737854a7f9e026ed259eb2d2cba5f4
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-27 10:15:51 -0300

    Fatorial de um número

---
+++ Lista de exercícios 1/exercicio4.py
@@ -0,0 +1,7 @@
+#4.	Faça um programa que calcule o fatorial de um número positivo informado pelo usuário.
+
+from math import factorial
+num = int(input('Informe um número positivo:'))
+print('O fatorial de {} é {}.' .format(num, factorial(num)))
+
+
