98c1b9163d55e266169f5395b8c214706218b894
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-27 14:02:38 -0300

    100 primeiros números ímpares

--- Lista de exercícios 1/exercicio2.py
+++ Lista de exercícios 1/exercicio2.py
@@ -1,8 +1,13 @@
-# 2.	Escreva um programa que imprima os 100 primeiros números ímpares.
-cont = -1
-qtde = 0
-for i in range(1,101):
-    cont = cont + 2
-    qtde = qtde + 1 #quantidade que o laço funciona
-    print(cont, end=' ')
-print('\nEsses são os {} primeiros números ímpares.'.format(qtde))
+# 2.Escreva um programa que imprima os 100 primeiros números ímpares.
+
+#fórmula do N-ésimo termo:
+#N-ésimo = primeiroNumero + (ultimoNumero - 1) * razão
+
+cont = 0
+x = 1 + (100 - 1) * 2
+for c in range(1, x + 2):
+    if c % 2 != 0:
+        print(c, end=' ')
+        cont += 1
+print('\nEsses são os {} primeiros números ímpares.'.format(cont))
+
