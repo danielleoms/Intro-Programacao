be9db64848d41dd02301d1bfda765d819bcb2e5b
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-29 20:33:14 -0300

    fórmula do cálculo da hipotenusa

--- AV1/1.py
+++ AV1/1.py
@@ -1,7 +1,7 @@
 #1.	Faça um programa que calcule e apresente o valor da hipotenusa “c” de um
 # triângulo retângulo, dado o valor de seus catetos “a” e “b”, segundo a fórmula:
 
-#obs: fórmula do cálculo da hipotenusa: a² + b² = c²
+#fórmula do cálculo da hipotenusa: a² + b² = c²
 
 cateto1 = float(input('Informe o valor do primeiro cateto:'))
 cateto2 = float(input('Informe o valor do segundo cateto:'))
