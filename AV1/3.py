c317529531d5ae4a04b7e4654c995fce33b5c6bc
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-29 16:33:15 -0300

    números ímpares de 1 a N

--- AV1/3.py
+++ AV1/3.py
@@ -1,5 +1,7 @@
-#3.	Faça um programa em Python que leia um número inteiro N menor que 1.000
-# e apresente todos os números ímpares de 1 a N, inclusive N
+#3.	Faça um programa em Python que leia um número inteiro N menor que 1.000 e apresente todos os números ímpares de 1 a N,
+# inclusive N
+
+# Utilizando while
 
 n = int(input("Digite um número menor que 1000:"))
 x = 0
@@ -10,3 +12,13 @@ else:
         if x % 2 != 0:
             print(x, end=' ')
         x = x + 1
+
+# Usando for
+
+n = int(input('\nInforme um número menor que 1000:'))
+if n > 1000:
+    print('Informe um número menor que 1000.')
+else:
+    for c in range(1, n+1):
+        if c % 2 != 0:
+            print(c, end=' ')
