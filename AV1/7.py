c1d9e8f65c177e507d75a86d6efde7e69c4523d6
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-29 15:04:50 -0300

    não terminei

---
+++ AV1/7.py
@@ -0,0 +1,2 @@
+# 7.	(Questão opcional - Desafio) Faça um programa em Python que leia um número inteiro N menor que 10.000
+#e apresente todos os números de 1 a N que divididos por N dão resto 2 (Dica: use uma lógica similar àquele problema apresentar os números pares).
