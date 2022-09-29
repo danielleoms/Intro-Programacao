a95711f2fa1b9b54eee705d0363ebe1ebed4f158
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-29 14:26:38 -0300

    não terminei

--- AV1/3.py
+++ AV1/3.py
@@ -1,8 +1,2 @@
 #3.	Faça um programa em Python que leia um número inteiro N menor que 1.000
 # e apresente todos os números ímpares de 1 a N, inclusive N
-
-
-while n <= 1000:
-    n = int(input('Insira um número'))
-    if n % 2 != 0:
-        print(n)
