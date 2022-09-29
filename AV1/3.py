6ca24f18c89cf3a7d4d46000ebd523830cf757c6
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-29 19:37:30 -0300

    Update 3.py

--- AV1/3.py
+++ AV1/3.py
@@ -1,21 +1,9 @@
 #3.	Faça um programa em Python que leia um número inteiro N menor que 1.000 e apresente todos os números ímpares de 1 a N,
 # inclusive N
 
-# Utilizando while
-
-n = int(input("Digite um número menor que 1000:"))
-x = 0
-if n > 1000:
-    print('Informe um número menor que 1000.')
-else:
-    while x <= n:
-        if x % 2 != 0:
-            print(x, end=' ')
-        x = x + 1
-
 # Usando for
 
-n = int(input('\nInforme um número menor que 1000:'))
+n = int(input('Informe um número menor que 1000:'))
 if n > 1000:
     print('Informe um número menor que 1000.')
 else:
