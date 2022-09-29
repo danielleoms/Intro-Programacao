fb66a9d2192587ae3dd7e357ad859b10f463b9a0
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-29 15:04:17 -0300

    não terminei

---
+++ AV1/5.py
@@ -0,0 +1,2 @@
+# 5.	Faça um programa em Python que leia um número inteiro “N”, 
+#calcule e apresente todos os seus divisores. (Dica: use uma estrutura while para testar todos os possíveis divisores)
