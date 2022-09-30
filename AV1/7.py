dd46cd1a361f9eb8060aea2cd336f2831a835e5f
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-30 15:17:51 -0300

    Desafio

--- AV1/7.py
+++ AV1/7.py
@@ -1,2 +1,16 @@
-# 7.	(Questão opcional - Desafio) Faça um programa em Python que leia um número inteiro N menor que 10.000
-#e apresente todos os números de 1 a N que divididos por N dão resto 2 (Dica: use uma lógica similar àquele problema apresentar os números pares).
+#7. Faça um programa em Python que leia um número inteiro N menor que 10.000
+#e apresente todos os números de 1 a N que divididos por N dão resto 2
+# (Dica: use uma lógica similar àquele problema apresentar os números pares).
+
+
+n = int(input('Digite um número inteiro menor que 10000:'))
+cont=1
+
+if n > 10000:
+    print('Por favor, insira um número menor que 10000.')
+else:
+    while cont <= 10000:
+        resto = cont % n
+        if resto == 2:
+            print(cont, end= ' ')
+        cont += 1
