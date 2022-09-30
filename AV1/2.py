19466b2704c17ae9acf99d9d460574783dc8cdca
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-30 11:14:40 -0300

    Triângulo retângulo

--- AV1/2.py
+++ AV1/2.py
@@ -1,13 +1,12 @@
 #2.	Faça um programa que leia os 3 lados de um triangulo e determine se ele é um triangulo retângulo.
 # Para isto utilize a fórmula do item anterior nesta checagem.
 
-cateto1 = float(input('Digite o valor do primeiro cateto: '))
-cateto2 = float(input('Digite o valor do segundo cateto: '))
-hi = float(input('Digite o valor da hipotenusa: '))
-((cateto1 ** 2) + (cateto2 ** 2)) ** (1/2)
+a = int(input('Digite o valor do primeiro segmento: '))
+b = int(input('Digite o valor do segundo segmento: '))
+c = int(input('Digite o valor do terceiro segmento: '))
 
-if hi == ((cateto1 ** 2) + (cateto2 ** 2)) ** (1/2):
-    print(f'Os lados {cateto1},{cateto2} e {hi} formam um triângulo retângulo')
+if c == ((a **2) + (b **2)) **(1/2) or a == ((c **2) + (b **2)) **(1/2) or b == ((a **2) + (c **2)) **(1/2):
+    print(f'Os lados {a},{b} e {c} formam um triângulo retângulo')
 
 else:
-    print(f'Os lados {cateto1},{cateto2} e {hi} não formam um triângulo retângulo')
+    print(f'Os lados {a},{b} e {c} NÃO formam um triângulo retângulo')
