a57e730dd0e060bca53db03fb6ac0c4319d0fc33
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-30 11:23:28 -0300

    Divisidores de n

--- AV1/5.py
+++ AV1/5.py
@@ -1,2 +1,12 @@
-# 5.	Faça um programa em Python que leia um número inteiro “N”, 
-#calcule e apresente todos os seus divisores. (Dica: use uma estrutura while para testar todos os possíveis divisores)
+#5. Faça um programa em Python que leia um número inteiro “N”, calcule e apresente todos os seus divisores.
+#(Dica: use uma estrutura while para testar todos os possíveis divisores)
+
+
+number = int(input('Insira um número: '))
+c = 1
+print(f'os divisores de {number} são: ')
+while c <= number:
+    resto  = number % c
+    if resto == 0:
+        print(c)
+    c += 1
