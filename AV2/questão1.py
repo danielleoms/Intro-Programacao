9d2f5e0f9895244fa1738bf7d931f5a5410bc21b
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-12-02 18:45:18 -0300

    Update questão1.py

--- AV2/questão1.py
+++ AV2/questão1.py
@@ -1,3 +1,7 @@
+#1.	Senhas Criticadas- Faça um programa em Python que leia uma senha
+#aleatória e forneça seu tamanho, e imprima uma versão maiúscula e
+#uma minúscula da senha. 
+
 import random, string
 
 print('Bem-vindo ao gerador de senhas!')
@@ -13,3 +17,11 @@ for index in range(senha_length):
 print("Senha gerada: {}".format(password))
 print("Versão maiúscula da senha: {}".format(password.upper()))
 print("Versão minúscula da senha: {}".format(password.lower()))
+
+#(opcional) – incremente o programa e valide uma senha segundo os seguintes critérios:
+# Tamanho – de 6 a 8 caracteres; 
+# Pelo menos 1 número; 
+# Não pode começar por número;
+# Pelo menos 1 letra maiúscula;
+
+password[0].isalpha() => true
