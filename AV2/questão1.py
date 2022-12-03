2f9104c1deac4f2b4934e75aeb29d4b38a209746
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-12-03 13:41:26 -0300

    Update questão1.py

--- AV2/questão1.py
+++ AV2/questão1.py
@@ -2,17 +2,8 @@
 #aleatória e forneça seu tamanho, e imprima uma versão maiúscula e
 #uma minúscula da senha. 
 
-import random, string
+password= str(input("Digite o tamanho da senha: "))
 
-print('Bem-vindo ao gerador de senhas!')
-senha_length = int(input("Digite o tamanho da senha: "))
-
-caracteres = string.ascii_letters + string.digits 
-
-password = ""   
-
-for index in range(senha_length):
-    password = password + random.choice(caracteres)
 
 print("Senha gerada: {}".format(password))
 print("Tamanho da senha:{}" .format(len(password)))
