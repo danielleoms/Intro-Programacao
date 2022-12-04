360f2f8545ff375f6d1602ac908211ea06c29db0
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-12-04 11:00:27 -0300

    Update questão1.py

--- AV2/questão1.py
+++ AV2/questão1.py
@@ -1,42 +1,39 @@
-#1- Senhas Criticadas- Faça um programa em Python que leia uma senha aleatória #forneça seu tamanho, e imprima uma versão maiúscula e uma minúscula da #senha. 
+#1- Senhas Criticadas- Faça um programa em Python que leia uma senha aleatória forneça seu tamanho, e imprima uma versão maiúscula e uma minúscula da senha.
 
-def lenght (p, min, max):
-  if min <= len(p) <= max:
-    return True
-  else:
-    return False
-    
-def valid1dig(p):
-    for i in p:
-        if i.isnumeric():
-            return True
-    return False
-    
-def no1number(p):
-    if p[0].isnumeric(): 
-        return True
-    else:
-        return False
-        
-def pelomenos1maisc(p):
-    for i in p:
-        if i.isupper():
-            return True
-    return False
-  
+password = input("Digite uma senha: ")
+lenght = len(password)
+passwordupper = password.upper()
+passwordlower = password.lower()
+start = password[0].isdigit()
+contador = 0
+cont = 0
 
-while True:
-    password= str(input("Digite sua senha: "))
-    if lenght(password, 6, 8 ) and valid1dig(password)  and no1number(password) and pelomenos1maisc(password):
-        break 
- #(opcional) – incremente o programa e valide uma senha segundo os seguintes critérios:
-    if not lenght(password, 6, 8 ):
-        print("SENHA INVÁLIDA! Sua senha deve ter entre 6 e 8 caracteres.")
-    if not no1number(password):
-        print("SENHA INVÁLIDA! Sua senha precisa começar com um número.")
-    if not pelomenos1maisc(password):
-        print("SENHA INVÁLIDA! Sua senha precisa ter pelo menos uma letra maiúscula.")
-        
-print("Sua senha contém: {} caracteres" .format(len(password)))
-print("Versão maiúscula da senha: {}".format(password.upper()))
-print("Versão minúscula da senha: {}".format(password.lower()))
+for x in range (0, len(password)):
+    number = password[x].isdigit()
+    if number == True:
+        contador = contador + 1
+
+for x in range(0, len(password)):
+    dupper = password[x].isupper()
+    if dupper == True:
+        cont = cont + 1
+#(opcional) – incremente o programa e valide uma senha segundo os seguintes critérios:
+
+if contador == 0:
+    print("Senha inválida")
+
+elif start == True:
+    print("Senha inválida")
+
+elif cont == 0:
+    print("Senha inválida")
+
+elif lenght < 6 or lenght > 8:
+    print("Senha inválida")
+
+else:
+    print("Senha válida")
+    print("Número de caracteres:",lenght)
+    print("Saída em maiúsculo:",passwordupper)
+    print("Saíde em minúsculo",passwordlower)
+    print("Sua senha:",password)
